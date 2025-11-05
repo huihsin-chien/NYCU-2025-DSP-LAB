5a87ae91407c54148b6b3c0a2f34e5822a41654c
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2025-11-05 14:26:35 +0800

    add LiDAR data & trimmed imu data

--- LAB03-20251015/trim_data.m
+++ LAB03-20251015/trim_data.m
@@ -1,7 +1,7 @@
 clear;       % 清除 workspace 中的變數
 clc;         % 清除 Command Window
 close all;   % 關閉所有 figure 視窗
-file = fullfile("walk","straight2.mat");
+file = fullfile("walk","straight3.mat");
 data = load(file).data;
 
 figure();
@@ -14,8 +14,11 @@ plot(data(:, 6)); % omega_z
 grid on;
 xlabel("sample point"); ylabel("omega_z");
 
-subwalk_1 = data(790:1709, :);
-subwalk_2 = data(1910:2895, :);
+data = data(3189:4228, :);
+
+%{
+subwalk_2 = data(1938:2927, :);     
+subwalk_3 = data(3189:4228, :);
 figure();
 subplot(2,1,1);
 plot(subwalk_1(:, 3)); % acc_z for subwalk_1
@@ -25,3 +28,4 @@ subplot(2,1,2);
 plot(subwalk_2(:, 3)); % acc_z for subwalk_2
 grid on;
 xlabel("sample point"); ylabel("acc_z (subwalk 2)");
+%}
